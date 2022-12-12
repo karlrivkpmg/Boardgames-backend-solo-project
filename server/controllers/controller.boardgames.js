7c4473cd684124e11b5bc366ce4b7c3bc90b4e6f
karlrivkpmg <karl.rivett@kpmg.co.uk>
2022-12-12 17:03:02 +0000

    Finish 4 GET/api/reviews with tests

--- server/controllers/controller.boardgames.js
+++ server/controllers/controller.boardgames.js
@@ -1,8 +1,15 @@
-const { selectCategories } = require('../models/model.boardgames')
+const { selectCategories, selectReviews } = require('../models/model.boardgames')
 
 exports.getCategories = (req, res) => {
     selectCategories()
     .then((categories)=>{
         res.status(200).send({categories});
+    })
+}
+
+exports.getReviews = (req, res) =>{
+    selectReviews()
+    .then((reviews)=>{
+        res.status(200).send({reviews});
     })
 }
