8722c5bbd220af9c8a5e1e9f54703ed6a86646e0
karlrivkpmg <karl.rivett@kpmg.co.uk>
2022-12-13 11:29:34 +0000

    more fixing

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const app = express();
 
 app.get('/api/categories', getCategories)
 
-app.get('api/reviews/:review_id', getReviewById);
 app.get('/api/reviews', getReviews);
 
 app.all('/*', handle404s);
