63c65e932e48650eb2c6c160ac82b5145a997d24
karlrivkpmg <karl.rivett@kpmg.co.uk>
2022-12-16 13:51:17 +0000

    tidy

--- sandbox.sql
+++ sandbox.sql
@@ -1,4 +1,8 @@
 \c nc_games_test
-select category, COUNT(category)
+SELECT title, designer, owner, review_img_url, category,reviews.created_at, reviews.votes, reviews.review_id, COUNT(comments.review_id) AS comment_count
 FROM reviews
-GROUP BY category;
+LEFT JOIN comments
+ON reviews.review_id = comments.review_id
+WHERE reviews.category = 'dexterity'
+GROUP BY title, designer, owner, review_img_url, category,reviews.created_at, reviews.votes, reviews.review_id
+ORDER BY created_at desc;
